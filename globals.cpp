8a81d3aba481b0adf6511dcb1746451deecfcd4d
Josh Camarena <joshcamarena@Josh-MBP.local>
2016-05-17 17:23:25 -0700

    globals for the project

---
+++ globals.cpp
@@ -0,0 +1,14 @@
+//
+//  Global defs 
+//
+//  Josh Camarena
+//
+
+
+#ifndef __reversabomb_globals_cpp__
+#define __reversabomb_globals_cpp__
+
+const int MAXLIVES = 7;
+
+
+#endif /* __reversabomb_globals_cpp__ */
