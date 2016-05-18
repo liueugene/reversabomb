3e719ed03301e8104c8d6cc7eee8b2b10744787a
Josh Camarena <joshcamarena@Josh-MBP.local>
2016-05-17 17:18:02 -0700

    first scoreboard implementation

---
+++ scoreboard.cpp
@@ -0,0 +1,52 @@
+//
+//  scoreboard.cpp
+//  
+//
+//  Created by Josh Camarena on 5/17/16.
+//
+//
+
+#include "scoreboard.hpp"
+#include <SPI.h>
+#include <Adafruit_DotStar.h>
+
+scoreboard::scoreboard(Adafruit_DotStar *strip)
+: m_strip(strip), m_leftLife(7), m_rightLife(7), m_rightBegin(7), m_rightEnd(15), m_leftBegin(0), m_leftEnd(7), update(true)
+{
+    this->update();
+}
+
+void scoreboard::loseLife(bool TEAM) //0 left, 1 right
+{
+    if(TEAM)
+        m_rightLife--;
+    
+    else
+        m_leftLife--;
+ 
+    m_update = true;
+    
+    return;
+}
+
+
+void scoreboard::update()
+{
+    if (!m_update)
+        return;
+    
+    for (int i = 0; i < MAXLIVES; i++)
+    {
+        if (i < m_rightLife)
+            strip->setPixelColor(i + m_rightBegin, 0x0000FF);
+        else
+            strip->setPixelColor(i + m_rightBegin, 0);
+        
+        if (i < m_leftLife)
+            strip->setPixelColor(i + m_leftBegin, 0xFF0000);
+        else
+            strip->setPixelColor(i + m_leftBegin, 0);
+    }
+    m_update = false;
+    return;
+}
---
+++ scoreboard.hpp
@@ -0,0 +1,35 @@
+//
+//  scoreboard.hpp
+//  
+//
+//  Created by Josh Camarena on 5/17/16.
+//
+//
+
+#ifndef scoreboard_hpp
+#define scoreboard_hpp
+
+class Adafruit_DotStar;
+
+class scoreboard
+{
+public:
+    scoreboard(Adafruit_DotStar *strip);
+    void loseLife(bool TEAM); //0 left, 1 right
+    void update();
+
+private:
+    Adafruit_DotStar *m_strip;
+    int m_rightLife;
+    int m_leftLife;
+    
+    bool m_update;
+    
+    int m_leftBegin;
+    int m_leftEnd;
+    int m_rightBegin;
+    int m_rightEnd;
+};
+
+
+#endif /* scoreboard_hpp */
